67a2b8d34d4a28ae80eef4a40d30070f92e952ed
collinshoromba <collinserver001@gmail.com>
2023-09-28 18:59:12 +0200

    commit menu

--- scripts/getDates.js
+++ scripts/getDates.js
@@ -3,3 +3,11 @@ let d = new Date();
 let newUpdate = document.getElementById('footer-sect');
 newUpdate.querySelector('#currentYear').innerHTML = d.getFullYear();
 newUpdate.querySelector('#currentDateAndTime').innerHTML = document.lastModified;
+
+const hamButton = document.querySelector('#menu');
+const navigation = document.querySelector('.navigation');
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+});
