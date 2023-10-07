9df7428520215cbbe8368820eb0dfba4d2be4a28
collinshoromba <collinserver001@gmail.com>
2023-10-07 23:24:28 +0200

    commit this

---
+++ chamber/scripts/chamber.js
@@ -0,0 +1,24 @@
+let d = new Date();
+
+let newUpdate = document.getElementById('footer-sect');
+newUpdate.querySelector('#currentYear').innerHTML = d.getFullYear();
+newUpdate.querySelector('#currentDateAndTime').innerHTML = document.lastModified;
+
+const btn = document.getElementById('menu-btn')
+const nav = document.getElementById('menu')
+
+function navToggle() {
+  btn.classList.toggle('open')
+  nav.classList.toggle('hidden')
+  document.body.classList.toggle('no-scroll')
+}
+
+btn.addEventListener('click', navToggle);
+
+//const hamButton = document.querySelector('#menu');
+//const navigation = document.querySelector('.navigation');
+
+//hamButton.addEventListener('click', () => {
+	//navigation.classList.toggle('open');
+	//hamButton.classList.toggle('open');
+//});
