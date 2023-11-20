23cc6a81fac8a93b238fddd96935d2b1cd279d70
collinshoromba <collinserver001@gmail.com>
2023-11-20 12:38:19 +0200

    commit this

--- scripts/weather.js
+++ scripts/weather.js
@@ -1,37 +1,50 @@
 document.addEventListener('DOMContentLoaded', function () {
     const apiKey = '807a06fe21a581ca08e4f84347eba1d9';
     const city = 'Vereeniging';
-    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}`;
-  
-    fetch(apiUrl)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => {
-        updateWeatherCard(data);
-      })
-      .catch(error => {
-        console.error('Error fetching weather data:', error);
-      });
-  
-    function updateWeatherCard(data) {
-      const temperatureKelvin = data.main.temp;
-      const temperatureCelsius = Math.round(temperatureKelvin - 273.15); // Convert Kelvin to Celsius and round to the nearest whole number
-      const description = data.weather[0].description;
-      const iconCode = data.weather[0].icon;
-  
-      document.getElementById('temperature').textContent = temperatureCelsius + ' °C';
-      document.getElementById('description').textContent = description;
-      document.getElementById('weather-icon').src = `https://openweathermap.org/img/w/${iconCode}.png`;
+
+    // Initial fetch and update
+    fetchWeatherData();
+
+    // Set interval to fetch and update weather data every 10 minutes (600,000 milliseconds)
+    const intervalId = setInterval(() => {
+        fetchWeatherData();
+    }, 600000);
+
+    function fetchWeatherData() {
+        const timestamp = new Date().getTime(); // Add a timestamp to prevent caching
+        const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&dt=${timestamp}`;
+
+        fetch(apiUrl)
+            .then(response => {
+                if (!response.ok) {
+                    throw new Error('Network response was not ok');
+                }
+                return response.json();
+            })
+            .then(data => {
+                console.log('Weather data:', data); // Log the response for debugging
+                updateWeatherCard(data);
+            })
+            .catch(error => {
+                console.error('Error fetching weather data:', error);
+            });
     }
 
-   // Function to capitalize the first letter of a string
-   function capitalizeFirstLetter(string) {
-    return string.charAt(0).toUpperCase() + string.slice(1);
-  }
- 
+    function updateWeatherCard(data) {
+        const temperatureKelvin = data.main.temp;
+        const temperatureCelsius = Math.round(temperatureKelvin - 273.15);
+        const description = capitalizeEachWord(data.weather[0].description);
+        const iconCode = data.weather[0].icon;
 
+        document.getElementById('temperature').textContent = temperatureCelsius + ' °C';
+        document.getElementById('description').textContent = description;
+        document.getElementById('weather-icon').src = `https://openweathermap.org/img/w/${iconCode}.png`;
+    }
+
+    // Function to capitalize the first letter of each word in a string
+    function capitalizeEachWord(string) {
+        return string.replace(/\b\w/g, c => c.toUpperCase());
+    }
 });
+
+
