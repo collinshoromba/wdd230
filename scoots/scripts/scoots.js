e94f3d8de5934786387eef8cd24426301ec757f7
collinshoromba <collinserver001@gmail.com>
2023-12-15 22:05:53 +0200

    commit this

--- scoots/scripts/scoots.js
+++ scoots/scripts/scoots.js
@@ -119,3 +119,22 @@ getWeatherData();
               document.getElementById('rentals').appendChild(container);
             });
           });
+
+
+
+
+          let d = new Date();
+
+          let newUpdate = document.getElementById('footer-sect');
+          newUpdate.querySelector('#currentYear').innerHTML = d.getFullYear();
+          newUpdate.querySelector('#currentDateAndTime').innerHTML = document.lastModified;
+          
+          
+          
+          const hamButton = document.querySelector('#menu');
+          const navigation = document.querySelector('.navigation');
+          
+          hamButton.addEventListener('click', () => {
+            navigation.classList.toggle('open');
+            hamButton.classList.toggle('open');
+          });
